463f973ffc6785756b289a08263eda66796ac96a
WebTechEkki <if20b012@technikum-wien.at>
2021-04-21 12:33:36 +0200

    implemente

--- rassplayer_backend/application/db_class.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine("mysql+pymysql://johnlennon:woodstock69@localhost:8008/raspplayer")
-session = sessionmaker(bind=engine)()
-base = declarative_base()
